bf8b6c9ed9b986f9efa67f081c6d313ac9347119
LeandroGal <lean.galante@gmail.com>
2021-04-02 16:56:26 -0300

    Empezando con el CRUD

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,77 @@
+<?php 
+
+    // Base de Datos 
+
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $imagen = $_POST['imagen'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedorId = $_POST['vendedorId'];
+    }
+
+
+    require '../../includes/funciones.php'; 
+    incluirTemplate('header');
+?>
+    <main class="contenedor seccion">
+        <h1>Crear</h1>
+
+        <a href="/admin/index.php" class="boton boton-verde">Volver</a>
+
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+            <fieldset>
+                <legend>Informacion General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" name="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">descripcion:</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Informacion Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" name="wc" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 1" min="1" max="5">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedorId">
+                    <option value="1">Leandro</option>
+                    <option value="2">Juan</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
+    </main>
+
+<?php 
+    incluirTemplate('footer');
+?>
